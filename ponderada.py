86a463bc1f4da533d45e4a78c655b33bffa1387b
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-02-24 15:02:05 -0300

    Update ponderada.py

--- ponderada.py
+++ ponderada.py
@@ -1,104 +1,62 @@
-from pydobot import *
-from serial.tools import list_ports
+import inquirer
 
-# Mandar para a posição home
-def home():
-    #m_lite.set_home()
-    print("casa")
-    menu()
+class Robo:
+    def __init__(self):
+        self.posicao = {"x": 0, "y": 0, "z": 0}
+        self.ferramenta_ligada = False
+
+    def home(self):
+        self.posicao = {"x": 0, "y": 0, "z": 0}
+        return "Robô retornou à posição inicial."
+
+    def ligar_ferramenta(self):
+        self.ferramenta_ligada = True
+        return "Ferramenta ligada."
+
+    def desligar_ferramenta(self):
+        self.ferramenta_ligada = False
+        return "Ferramenta desligada."
+
+    def mover(self, eixo, distancia):
+        if eixo.lower() in ["x", "y", "z"]:
+            self.posicao[eixo.lower()] += distancia
+            return f"Robô movido {distancia} unidades no eixo {eixo}."
+        else:
+            return "Eixo inválido. Use 'x', 'y' ou 'z'."
+
+    def atual(self):
+        return f"Posição atual do robô: {self.posicao}, Ferramenta ligada: {self.ferramenta_ligada}"
 
 
-
-# Ligar a ferramenta (atuador)
-def ligarFerramenta():
-    if head == "gripper":
-        print("gripClose")
-        #m_lite.set_endeffector_gripper(enable=True, on=True)
+def execute_comando(robo, comando):
+    if comando == "home":
+        return robo.home()
+    elif comando == "ligar_ferramenta":
+        return robo.ligar_ferramenta()
+    elif comando == "desligar_ferramenta":
+        return robo.desligar_ferramenta()
+    elif comando == "mover":
+        eixo = inquirer.prompt([inquirer.List("eixo", message="Escolha o eixo", choices=["x", "y", "z"])])["eixo"]
+        distancia = inquirer.prompt([inquirer.Text("distancia", message="Digite a distância")])["distancia"]
+        return robo.mover(eixo, int(distancia))
+    elif comando == "atual":
+        return robo.atual()
     else:
-        print("succStart")
-        #m_lite.set_endeffector_suctioncup(enable=True, on=True)
-    menu()
+        return "Comando inválido."
 
-# Desligar a ferramenta (atuador)
-def desligarFerramenta():
-    if head == "gripper":
-        print("gripOpen")
-        #m_lite.set_endeffector_gripper(enable=True, on=False)
-    else:
-        print("succOff")
-        #m_lite.set_endeffector_suctioncup(enable=True, on=False)
-    menu()
-
-def quit():
-    exit(1)
-
-def obter_posicao_atual():
-    # Implementa a função para obter a posição atual do robô
-    # pos = m_lite.pose()
-    # print(f"Posição Atual: X={pos[0]}, Y={pos[1]}, Z={pos[2]}")
-    print("Posição Atual: X=0, Y=0, Z=0")  # Simulação, substitua pelos valores reais
-
-    menu()
-
-def mover_eixo(axis, distance):
-    # Implementa a função para mover o robô em um eixo específico (x, y ou z)
-    if axis.lower() == 'x':
-        # Mover na direção X
-        # m_lite.move_to(x=current_x + distance, y=current_y, z=current_z)
-        print(f"Moveu {distance} unidades em X.")
-    elif axis.lower() == 'y':
-        # Mover na direção Y
-        # m_lite.move_to(x=current_x, y=current_y + distance, z=current_z)
-        print(f"Moveu {distance} unidades em Y.")
-    elif axis.lower() == 'z':
-        # Mover na direção Z
-        # m_lite.move_to(x=current_x, y=current_y, z=current_z + distance)
-        print(f"Moveu {distance} unidades em Z.")
-    else:
-        print("Eixo inválido.")
-
-    menu()
-    
-def menu():
-    print("\nO que você quer fazer?")
-    inter = str(input())
-    match inter:
-        case "ligar":
-            ligarFerramenta()
-        case "desligar":
-            desligarFerramenta()
-        case "home":
-            home()
-        case "mover":
-            eixo = str(input("Qual eixo (X, Y ou Z)? "))
-            distancia = float(input("Distância a percorrer: "))
-            mover_eixo(eixo, distancia)
-        case "atual":
-            obter_posicao_atual()
-        case "quit":
-            quit()
-        case _:
-            quit()
-
-head = None
 
 if __name__ == "__main__":
-    available_ports = list_ports.comports()
-    try:
-        port = available_ports[0].device
-        device = pydobot.Dobot(port=port, verbose=True)
-    except:
-        port = available_ports[1].device
-        device = pydobot.Dobot(port=port, verbose=True)
-    print("Olá! esta é uma pequena interface gráfica para interagir com o robô")
-    head = str(input("\nInicialmente, qual o atuador sendo utilizado?\n"))
-    # available_ports = list_ports.comports()
-    # print(f'available ports: {[x.device for x in available_ports]}')
-    # port = available_ports[0].device
-    # a = int(input("Quantos inputs vc quer? "))
-    # b = []
-    # for i in range(a):
-    #     b.append(str(input(f'Frase {i + 1}:')))
-    
-    # print(b)
-    menu()
+    robo = Robo()
+
+    while True:
+        perguntas = [
+            inquirer.List("comando", message="Escolha um comando", choices=["home", "ligar_ferramenta", "desligar_ferramenta", "mover", "atual", "sair"])
+        ]
+
+        resposta = inquirer.prompt(perguntas)
+
+        if resposta["comando"] == "sair":
+            break
+
+        resultado = execute_comando(robo, resposta["comando"])
+        print(resultado)
